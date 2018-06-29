40fa2ad2a1c5b0311f80d0b00be093476ef022e6
Glyn L <glyn_92@hotmail.com>
2018-06-29 17:38:06 +0700

    D5 - flex gallery

---
+++ 05 - Flex Panel Gallery/scripts.js
@@ -0,0 +1,23 @@
+const panels = document.querySelector(".panels");
+const allPanels = document.querySelectorAll(".panel");
+
+panels.style.display = "flex";
+panels.style.flexDirection = "row";
+
+allPanels.forEach(panel => {
+  panel.style.flex = "1 1 0";
+  panel.style.display = "flex";
+  panel.style.justifyContent = "center";
+  panel.style.flexDirection = "column";
+  panel.addEventListener("click", adjustSize);
+});
+
+function adjustSize(e) {
+  this.classList.toggle("open");
+  this.children[0].style.transform = "translateY(-50vh)";
+  this.children[0].style.visibility = "visible";
+  this.children[0].style.transform = "translateY(0)";
+  this.children[2].style.transform = "translateY(50vh)";
+  this.children[2].style.visibility = "visible";
+  this.children[2].style.transform = "translateY(0)";
+}
